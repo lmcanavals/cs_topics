741264820eadf694a6d8aa1a99e4dd29d5c43526
Luis M Canaval S <public02@canaval.org>
2023-10-19 10:08:56 -0500

    jade con maven funcionando

---
+++ week11/JadeTest.java
@@ -0,0 +1,4 @@
+import jade.core.Agent;
+
+public class JadeTest extends Agent {
+}
---
+++ week11/jade-test/src/main/java/cstopics/App.java
@@ -0,0 +1,13 @@
+package cstopics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
